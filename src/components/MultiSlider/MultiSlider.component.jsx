11762c54af972d33f7a731afcc131765e7485ac1
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-09-27 12:53:16 -0400

    created ref variables in multislider component

--- src/components/MultiSlider/MultiSlider.component.jsx
+++ src/components/MultiSlider/MultiSlider.component.jsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import './MultiSlider.styles.css';
+import PropTypes from 'prop-types';
 
-function MultiSlider() {
-	return <div>MultiSlider</div>;
+function MultiSlider({ min, max, onChange }) {
+	// Creating the state variables
+	const [minVal, setMinVal] = useState(min);
+	const [maxVal, setMaxVal] = useState(max);
+
+	// Creating the refs
+	const minValRef = useRef(min);
+	const maxValRef = useRef(max);
+
+	return (
+		<>
+			<input
+				type='range'
+				min={minVal}
+				max={maxVal}
+				value={minVal}
+				onChange={(event) => {
+					const value = Math.min(
+						Number(event.target.value),
+						maxVal - setMinVal(value)
+					);
+					setMinVal(value);
+					minValRef.current = value;
+				}}
+				className='thumb thumb--left'
+				style={{ zIndex: (minVal > max - 100) & '5' }}
+			/>
+			<input
+				type='range'
+				min={minVal}
+				max={maxVal}
+				value={maxVal}
+				onChange={(event) => {
+					const value = Math.max(
+						Number(event.target.value),
+						minVal - setMaxVal(value)
+					);
+					maxValRef.current = value;
+				}}
+				className='thumb thumb--right'
+			/>
+			<div className='slider'>
+				<div className='slider__track'></div>
+				<div className='slider__range'></div>
+			</div>
+		</>
+	);
 }
+
+MultiSlider.propTypes = {
+	min: PropTypes.number.isRequired,
+	max: PropTypes.number.isRequired,
+	onChange: PropTypes.func.isRequired,
+};
 
 export default MultiSlider;
